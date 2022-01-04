0143cccc04c6206a93a01e6d4794f0f453a0f56e
mungdo <annmunju@gmail.com>
2022-01-04 22:30:14 +0900

    오늘연습문제도 끝!

---
+++ pythonStudy/Day11/연습문제.py
@@ -0,0 +1,58 @@
+# 1.
+f = lambda x, y : x ** y
+
+# 2.
+ex = [1, 2, 3, 4, 5]
+func = list(map(lambda x : x **2 , ex))
+print(func)
+
+# 3. (3)
+
+# 4. (3)
+
+# 5. (2)
+
+# 6. import fah_convertor as fah
+
+# 7. from ..game import *
+
+# 8. (가) a    (나) write
+
+# 9. (가) w    (나) a    (다) r
+
+# 10. from calculator import *
+
+# 11.
+# 7
+# 3
+# 2
+# 1
+# 1
+# 1
+# 종료되었습니다.
+
+# 12. (5)
+
+# 13. (4)
+
+# 14. (4)
+
+# 15. (2)
+
+# 16. 숫자가 아닙니다.
+
+# 17.
+# (1) NameError
+# (2) FileNotFoundError
+# (3)
+# (4) KeyError
+
+# 18.
+# Not divided by 0
+# 1 3
+# 2 1
+
+# 19.
+with open("i_have_a_dream.txt", "r") as f :
+    contents = f.read()
+    print(contents)
